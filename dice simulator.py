e0be51a4dd373cbe64213d235f46d898bf6c238b
DeadPixels-cmd <77091711+DeadPixels-cmd@users.noreply.github.com>
2021-01-21 18:01:00 +0800

    Add files via upload
    Dice SImulator in Python (simple program)

---
+++ dice simulator.py
@@ -0,0 +1,45 @@
+import random
+print("This is a Dice Game!")
+confirm = "y"
+while confirm == "y":
+    number = random.randint(1,6)
+    if number == 1:
+        print("-----------")
+        print("|         |")
+        print("|    O    |")
+        print("|         |")
+        print("-----------")
+    if number == 2:
+        print("-----------")
+        print("|         |")
+        print("|  O   O  |")
+        print("|         |")
+        print("-----------")
+    if number == 3:
+        print("-----------")
+        print("|O        |")
+        print("|    O    |")
+        print("|        O|")
+        print("-----------")
+    if number == 4:
+        print("-----------")
+        print("|  O   O  |")
+        print("|         |")
+        print("|  O   O  |")
+        print("-----------")
+    if number == 5:
+        print("-----------")
+        print("|O       O|")
+        print("|    O    |")
+        print("|O       O|")
+        print("-----------")
+    if number == 6:
+        print("-----------")
+        print("|  O   O  |")
+        print("|  O   O  |")
+        print("|  O   O  |")
+        print("-----------")
+    confirm = input("Press y to roll again")
+    
+
+confirm = input("Press enter to roll again")
